df550334cd76aee9ef300acaa896166acb6a4698
Squierx <merenda.franco83@gmail.com>
2018-04-08 03:49:06 -0300

    Clase GeneradorTiempos con todos los métodos ya implementados.

--- src/fel/GeneradorTiempos.java
+++ src/fel/GeneradorTiempos.java
@@ -12,10 +12,28 @@ public class GeneradorTiempos {
     }
 	
     public static int getTiempoEntreArribos(){
-        
+        double numeroRandom=random.nextDouble();
+        if(numeroRandom <= 0.4)
+            return 4; //Para acumulada de 0.4 => 4 Minutos.
+        else
+            {
+                if (numeroRandom <= 0.7)
+                    return 5; //Para acumulada de 0.7 => 5 Minutos.
+                else
+                    return 6; //Para acumulada de 1 => 6 Minutos
+            }
     }
 	
     public static int getTiempoDuracionServicio(){
-        
+        double numeroRandom=random.nextDouble();
+        if(numeroRandom <= 0.5)
+            return 3; //Para acumulada de 0.5 => 3 Minutos.
+        else
+            {
+                if (numeroRandom <= 0.9)
+                    return 4; //Para acumulada de 0.9 => 4 Minutos.
+                else
+                    return 5; //Para acumulada de 1 => 5 Minutos
+            }
     }
 }
