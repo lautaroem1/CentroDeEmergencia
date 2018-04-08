bb2e6b12d4d13dd1cebef25f3662ac6eb74c123f
Squierx <merenda.franco83@gmail.com>
2018-04-08 13:42:55 -0300

    Implementación evento Fin, REVISAR

--- src/eventos/EventoFinSimulacion.java
+++ src/eventos/EventoFinSimulacion.java
@@ -6,8 +6,8 @@ import hospital.Servidor;
 public class EventoFinSimulacion extends Evento {
 
     public EventoFinSimulacion(float tiempo) {
-        super(2,tiempo,new Item(-1,));
-    }
+        super((byte) 2,tiempo,new Item(-1,10080));
+    }//Tiempo Arribo entiendo que va ser el final(10080 minutos en una semana)
 
     public void planificarEvento(Servidor servidor, Queue queue) {
         /* Cuando termina la simulacion no hacer nada */
