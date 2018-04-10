341c28d32f80169c58e6c91821e98a3c1d6c3ef5
Squierx <merenda.franco83@gmail.com>
2018-04-10 04:24:37 -0300

    Calculo de las estadisticas pedidas por consigna

--- src/hospital/Estadisticas.java
+++ src/hospital/Estadisticas.java
@@ -1,10 +1,18 @@
 package hospital;
 
 public class Estadisticas {
+    static float tiempoEsperaMedio=0;
+    static float porcentajeTiempoOcioso=0;
+    static float tiempoMedioTransito=0;
 
     // Ver donde y como implementar el calculo final.
     public static void calcularEstadisticas(float tiempoEsperaCola, float tiempoTransito, float tiempoOcioso,
                                             float tiempoFinSimulacion, int cantidadItems) {
-
+        /*Tiempo espera medio por paciente*/
+        tiempoEsperaMedio=((float)cantidadItems)/tiempoEsperaCola;
+        /*Porcentaje de tiempo Ocioso del médico*/
+        porcentajeTiempoOcioso=(tiempoOcioso/tiempoFinSimulacion);
+        /*Tiempo medio de tránsito por paciente*/
+        tiempoMedioTransito=((float)cantidadItems)/tiempoTransito;
     }
 }
