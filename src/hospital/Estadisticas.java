9c34efc50dde59f602207eafe6ac663984deecba
Squierx <merenda.franco83@gmail.com>
2018-04-10 10:12:39 -0300

    Calculo de estadisticas implementado

--- src/hospital/Estadisticas.java
+++ src/hospital/Estadisticas.java
@@ -8,11 +8,24 @@ public class Estadisticas {
     // Ver donde y como implementar el calculo final.
     public static void calcularEstadisticas(float tiempoEsperaCola, float tiempoTransito, float tiempoOcioso,
                                             float tiempoFinSimulacion, int cantidadItems) {
+
+        /*Carteles que luego se eliminan, es para controlar si está funcionando todo bien*/
+        System.out.println("Tiempo Espera cola = "+tiempoEsperaCola);
+        System.out.println("Tiempo Transito= "+tiempoTransito);
+        System.out.println("Tiempo Ocioso= "+tiempoOcioso);
+        System.out.println("Tiempo fin Simulacion= "+tiempoFinSimulacion);
+        System.out.println("Cantidad de items="+cantidadItems);
+        /*Fin Carteles que luego se eliminan, es para controlar si está funcionando todo bien*/
+
         /*Tiempo espera medio por paciente*/
-        tiempoEsperaMedio=((float)cantidadItems)/tiempoEsperaCola;
+        /*Si en la simulación no hubieran habido tiempos de espera, que no genere un valor inválido*/
+        if(tiempoEsperaCola!=0)
+            tiempoEsperaMedio=((float)cantidadItems)/tiempoEsperaCola;
+
         /*Porcentaje de tiempo Ocioso del médico*/
         porcentajeTiempoOcioso=(tiempoOcioso/tiempoFinSimulacion);
         /*Tiempo medio de tránsito por paciente*/
         tiempoMedioTransito=((float)cantidadItems)/tiempoTransito;
+
     }
 }
