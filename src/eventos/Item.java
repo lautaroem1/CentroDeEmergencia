c5faa2984bbbd5c8bc3f9cfd359c1a79254ade06
Squierx <merenda.franco83@gmail.com>
2018-04-10 04:12:54 -0300

    Commit para aclarar commit N°683d8f221710dfb4f919fb0746c41d887187f259 o commit anterior a este.
    Están todas las colecciones de estadisticas implementadas.
    *Tiempo ocioso
    *Tiempo espera
    *Tiempo tránsito
    *Cantidad Items
    *Tiempo fin simulacion

--- src/eventos/Item.java
+++ src/eventos/Item.java
@@ -30,6 +30,7 @@ public class Item {
     public static void setTiempoEsperaCola(float tiempoActual, float tiempoDuracionServicio, float tiempoArribo) {
         // El tiempo de espera en cola total es igual a la sumatoria de todos los tiempos de espera en cola.
 
+
         // Cada tiempo de cola se calcula con el tiempo actual,
         // que seria el tiempo en el que termina de atenderse, menos el tiempo en el que llego menos el tiempo de duracion de servicio.
         tiempoEsperaCola += tiempoActual - (tiempoDuracionServicio + tiempoArribo);
