831a4e1591a9659346558b86be577052176a5be0
Squierx <merenda.franco83@gmail.com>
2018-04-10 10:13:39 -0300

    Coleccion de estadisticas en Evento de Salida implementado

--- src/eventos/EventoSalida.java
+++ src/eventos/EventoSalida.java
@@ -22,26 +22,17 @@ public class EventoSalida extends Evento {
 
         } else {
             // De lo contrario, actualizar la cola y planificar proxima Salida con un nuevo Tiempo
-            // TODO: 8/4/2018 Queue debe reducirse en 1 y actualizar su cantidad.
             /*Saco item de la cola*/
             /*Genero evento de salida con itemActual*/
+
             Fel.getFel().insertarFel(new EventoSalida(this.getTiempo() + GeneradorTiempos.getTiempoDuracionServicio(), queue.suprimirCola()));
 
-            // TODO: 8/4/2018 Implementar coleccion de Estadisticas
-
-            /*Colecto tiempo en espera*/
-            Item.setTiempoEsperaCola(this.getTiempo(),this.getItem().getTiempoDuracionServicio(),this.getItem().getTiempoArribo());
-
-            /*Colecto tiempo en tránsito*/
-            Item.setTiempoTransito(this.getTiempo(),this.getItem().getTiempoArribo());
-
-
-
-
-
-
-
-
         }
+        /*Colecto tiempo en espera*/
+        System.out.println("TIEMPO COLA ACTUAL: "+(this.getTiempo() - (this.getItem().getTiempoDuracionServicio() + this.getItem().getTiempoArribo())));
+        Item.setTiempoEsperaCola(this.getTiempo(),this.getItem().getTiempoDuracionServicio(),this.getItem().getTiempoArribo());
+
+        /*Colecto tiempo en tránsito*/
+        Item.setTiempoTransito(this.getTiempo(),this.getItem().getTiempoArribo());
     }
 }
